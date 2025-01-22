09c3dca0222ac860704dcfe411e60285c91a8ca4
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-01-22 18:42:25 +0900

    feat Index Script
    Index Script 생성

---
+++ HEHE/Assets/Scripts/Index.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Index : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    void Start()
+    {
+        textMeshPro.text = "0";
+    }
+
+}
